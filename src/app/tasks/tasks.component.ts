2a34ed54cba6b12803f8af90d838a527f06ca721
Andre Neubert <ndre_luis_neubert@hotmail.com>
2025-09-16 21:35:40 -0300

    creating the new tasks section, using two-way data binding on the form, creating the custom events to create a new task to the specific user that triggered the request, displaying the new task on the tasks component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,25 +1,46 @@
 import { Component, Input } from '@angular/core';
-import { DUMMY_TASKS } from '../dummy-tasks';
+import { NewTask } from '../interfaces/new-task.model';
+import { User } from '../interfaces/user.model';
+import { DUMMY_TASKS } from './../dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 import { TaskComponent } from './task/task.component';
-
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
-}
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) user?: User;
+  isAddingTask = false;
   public tasks = DUMMY_TASKS;
 
   get selecterUserTasks() {
     return this.tasks.filter((task) => task.userId === this.user?.id);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onCreateNewTask(newTaskData: NewTask) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.user!.id,
+      title: newTaskData.title,
+      summary: newTaskData.summary,
+      dueDate: newTaskData.date,
+    });
+    this.isAddingTask = false;
   }
 }
